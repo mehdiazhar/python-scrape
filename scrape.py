6f92d2490d8cb6628e0dd4c294e681b4c9f7a547
Mehdi <m.mehdiazhar@gmail.com>
2022-01-29 20:15:37 +0500

    Getting value of string between div tags

--- scrape.py
+++ scrape.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 html = requests.get("https://mcdonalds.com.pk/our-menu/").text
 
 soup = BeautifulSoup(html, 'html.parser')
-print(soup.findAll('div', {"class": "content-title"})) 
-#print(soup.prettify())
+#print(soup.findAll('div', {"class": "content-title"}))
+for str in soup.findAll('div', {"class" : "content-title"}):
+    print(str.text)
+# print(soup.prettify())
 #print(html.text)
